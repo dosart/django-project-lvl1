077270c3c481d535f3e3df4987780c7d4a33b76c
Denis <Dysprosium92@ya.ru>
2021-12-13 20:07:15 +0300

    Added new page 'Password' to show password

--- generator/views.py
+++ generator/views.py
@@ -1,5 +1,10 @@
+from django.http import request
 from django.shortcuts import render
 
 
 def home(request):
     return render(request, 'generator/home.html', {'password': 'Qwerty1234'})
+
+def password(request):
+    return render(request, 'generator/password.html', {'password': 'Qwerty1234'})
+

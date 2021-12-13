482615e030d474ec1f71a82ab2caed1d819cf209
Denis <Dysprosium92@ya.ru>
2021-12-13 21:00:47 +0300

    Implemented: password generator uses random alphabet symbols to generate passwords.

--- generator/views.py
+++ generator/views.py
@@ -1,10 +1,20 @@
 from django.http import request
 from django.shortcuts import render
 
+import random
+
 
 def home(request):
     return render(request, 'generator/home.html', {'password': 'Qwerty1234'})
 
 def password(request):
-    return render(request, 'generator/password.html', {'password': 'Qwerty1234'})
+    password = ''
+
+    characters = list('qwertyuiopasdfghjklzxcvbnm')
+
+    length = 10
+    for _ in range(length):
+        password += random.choice(characters)
+
+    return render(request, 'generator/password.html', {'password': password})
 
